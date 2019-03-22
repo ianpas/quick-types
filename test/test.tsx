23d60aa98dfef8f72369705a8e8915c31295f019
ianpas <e78e78e78@126.com>
2019-03-22 08:53:02 +0800

    创建项目

---
+++ test/test.tsx
@@ -0,0 +1,8 @@
+//
+const div = <div id="1" class="div"></div>;
+
+//
+const input = <input id="1" class="input" type="text" value="hi" placeholder="placeholder"
+    onchange={(e: QuickApp.InputChangeEvent) => { }}
+    onclick={(e: QuickApp.ClickEvent) => { }}
+/>;
